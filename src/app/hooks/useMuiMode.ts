157bc69642b6e0cd9a1d32f960fe0d5b2ae8e1a2
Stephen <stepan.kopylov@ibitcy.com>
2023-03-31 19:06:44 +0300

    fix: exclude folders from color names

--- src/app/hooks/useMuiMode.ts
+++ src/app/hooks/useMuiMode.ts
@@ -1,13 +1,17 @@
 import { useColorScheme } from '@mui/joy/styles';
-import { Mode } from '@mui/system/build/cssVars/useCurrentColorScheme';
 import { useMutationObserver } from 'rooks';
 
 const htmlRef = {
   current: document.getElementsByTagName('html')[0],
 };
 
+export enum Mode {
+  light = 'light',
+  dark = 'dark',
+}
+
 function getModeFromHTMLTag(): Mode {
-  return htmlRef.current.classList.contains('figma-dark') ? 'dark' : 'light';
+  return htmlRef.current.classList.contains('figma-dark') ? Mode.dark : Mode.light;
 }
 
 export function useMuiMode(): void {
