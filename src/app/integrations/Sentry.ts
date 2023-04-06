e236e3febe8010d956e5aee0022bb8bba0ffc842
Jon <jon.sakas@gmail.com>
2023-04-05 20:31:21 -0600

    refactor: use webpack global for package version

--- src/app/integrations/Sentry.ts
+++ src/app/integrations/Sentry.ts
@@ -1,10 +1,8 @@
 import * as Sentry from '@sentry/react';
 
-import PackageJSON from '@root/package.json';
-
 Sentry.init({
   dsn: 'https://d9d5ce38f7404249a449339bc4bda34f@o203774.ingest.sentry.io/6701082',
-  release: PackageJSON.version,
+  release: PACKAGE_VERSION,
   beforeSend(event) {
     if (SENTRY_ENABLED === true) {
       event.request.url = undefined;
