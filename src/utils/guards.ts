3471a2de0d53a005979f831f50b5f3db79ae50f4
Jon Sakas <jon.sakas@community.com>
2023-11-24 09:36:00 -0700

    feat: support new object types
    Support for other object types besides rectangle:
    
    - Ellipse
    - Frame
    - Text
    - Line
    - Polygon
    - Star
    - Vector

--- src/utils/guards.ts
+++ src/utils/guards.ts
@@ -15,6 +15,25 @@ export function isImagePaint(paint: Paint): paint is ImagePaint {
   return paint.type === 'IMAGE';
 }
 
+export function isUsableNode(
+  node: SceneNode
+): node is EllipseNode | RectangleNode | FrameNode | TextNode | LineNode | PolygonNode | StarNode | VectorNode {
+  return (
+    node.type === 'ELLIPSE' ||
+    node.type === 'RECTANGLE' ||
+    node.type === 'FRAME' ||
+    node.type === 'TEXT' ||
+    node.type === 'LINE' ||
+    node.type === 'POLYGON' ||
+    node.type === 'STAR' ||
+    node.type === 'VECTOR'
+  );
+}
+
+export function isEllipseNode(node: SceneNode): node is EllipseNode {
+  return node.type === 'ELLIPSE';
+}
+
 export function isRectangleNode(node: SceneNode): node is RectangleNode {
   return node.type === 'RECTANGLE';
 }
@@ -25,6 +44,22 @@ export function isFrameNode(node: SceneNode): node is FrameNode {
 
 export function isTextNode(node: SceneNode): node is TextNode {
   return node.type === 'TEXT';
+}
+
+export function isLineNode(node: SceneNode): node is LineNode {
+  return node.type === 'LINE';
+}
+
+export function isPolygonNode(node: SceneNode): node is PolygonNode {
+  return node.type === 'POLYGON';
+}
+
+export function isStarNode(node: SceneNode): node is StarNode {
+  return node.type === 'STAR';
+}
+
+export function isVectorNode(node: SceneNode): node is VectorNode {
+  return node.type === 'VECTOR';
 }
 
 export function isGroupNode(node: SceneNode): node is GroupNode {
